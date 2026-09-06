47b027b2cf8e47dcf1cf72c917ccef9214bf1259
ccneko <clairechung112@gmail.com>
2026-09-07 02:53:59 +0900

    chore(core): export get_cluster_color_map and rank_cell_state in core module

--- clairescope/core/__init__.py
+++ clairescope/core/__init__.py
@@ -1,1 +1,18 @@
-"""CLAIREscope module initialization."""
+"""CLAIREscope core schema and analytical engines."""
+from .schema import (
+    get_gene_display_mappings,
+    resolve_gene_var_name,
+    get_annotation_columns,
+    get_sample_column,
+    get_cluster_color_map,
+    rank_cell_state,
+)
+
+__all__ = [
+    "get_gene_display_mappings",
+    "resolve_gene_var_name",
+    "get_annotation_columns",
+    "get_sample_column",
+    "get_cluster_color_map",
+    "rank_cell_state",
+]
